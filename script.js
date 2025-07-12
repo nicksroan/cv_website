a2d9239006c80901f11d3a8bce62c6b98df3a169
nicksroan <nicksroan@gmail.com>
2025-07-12 20:16:33 +0100

    blur effect every time

--- script.js
+++ script.js
@@ -22,6 +22,8 @@ const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
       entry.target.classList.add("visible");
+    } else {
+      entry.target.classList.remove("visible");
     }
   });
 }, observerOptions);
